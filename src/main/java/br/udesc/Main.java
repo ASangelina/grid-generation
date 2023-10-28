787ae91ad88782451e26a3f76b8d596ac27d97b3
Tiago <tiago.fernandes.godoy@gmail.com>
2023-10-28 01:21:32 -0300

    Added javalin dependencie and configured maven packaging

--- src/main/java/br/udesc/Main.java
+++ src/main/java/br/udesc/Main.java
@@ -1,7 +1,15 @@
 package br.udesc;
 
+import io.javalin.Javalin;
+
+@SuppressWarnings({"resource"})
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var app = Javalin.create().start(8080);
+
+        app.get("/generator", ctx -> ctx.json("Rota que deve retornar um componente vue!"));
+
+        app.post("/api/grid/generator", ctx -> ctx.json("Rota que deve executar o algorítimo!"));
     }
 }
