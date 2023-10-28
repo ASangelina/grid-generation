fccf4b1c572a7654b5ba34cd6adfeff69f10fd00
Angelina <machadoangelina321@gmail.com>
2023-10-28 20:54:11 -0300

    add  initial entities

---
+++ src/main/java/br/udesc/entities/DisciplineInitial.java
@@ -0,0 +1,79 @@
+public class Discipline {
+    private String disciplineCode;
+    private String description;
+    private String professorCode;
+    private int workHours;
+    private int credits;
+    private String classRestriction;
+    private String timeRestriction;
+
+    public Discipline(String disciplineCode, String description, String professorCode, int workHours, int credits,
+                      String classRestriction, String timeRestriction) {
+        this.disciplineCode = disciplineCode;
+        this.description = description;
+        this.professorCode = professorCode;
+        this.workHours = workHours;
+        this.credits = credits;
+        this.classRestriction = classRestriction;
+        this.timeRestriction = timeRestriction;
+    }
+
+    // Getters and Setters
+    public String getDisciplineCode() {
+        return disciplineCode;
+    }
+
+    public void setDisciplineCode(String disciplineCode) {
+        this.disciplineCode = disciplineCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getProfessorCode() {
+        return professorCode;
+    }
+
+    public void setProfessorCode(String professorCode) {
+        this.professorCode = professorCode;
+    }
+
+    public int getWorkHours() {
+        return workHours;
+    }
+
+    public void setWorkHours(int workHours) {
+        this.workHours = workHours;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public String getClassRestriction() {
+        return classRestriction;
+    }
+
+    public void setClassRestriction(String classRestriction) {
+        this.classRestriction = classRestriction;
+    }
+
+    public String getTimeRestriction() {
+        return timeRestriction;
+    }
+
+    public void setTimeRestriction(String timeRestriction) {
+        this.timeRestriction = timeRestriction;
+    }
+
+    
+}
---
+++ src/main/java/br/udesc/entities/ProfessorInitial.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProfessorInitial {
+    private int code;
+    private String name;
+    private List<String> disciplines;
+    private List<String> restrictedDays;
+
+    public Professor(int code, String name, List<String> disciplines, List<String> restrictedDays) {
+        this.code = code;
+        this.name = name;
+        this.disciplines = disciplines;
+        this.restrictedDays = restrictedDays;
+    }
+
+    // Getters and Setters
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getDisciplines() {
+        return disciplines;
+    }
+
+    public void setDisciplines(List<String> disciplines) {
+        this.disciplines = disciplines;
+    }
+
+    public List<String> getRestrictedDays() {
+        return restrictedDays;
+    }
+
+    public void setRestrictedDays(List<String> restrictedDays) {
+        this.restrictedDays = restrictedDays;
+    }
+
+    
+}
